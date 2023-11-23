8f2f061e4374a11db7bb3dc31627bd7848a499bd
Faaez Kamal <faaez.kamal@gmail.com>
2023-11-24 00:27:47 +1100

    Added leaderboard command and function to call functions in python

---
+++ commands/valorant/leaderboard.js
@@ -0,0 +1,48 @@
+const { SlashCommandBuilder } = require('discord.js');
+const lib = require('../../python_calls');
+const { fp } = require('../../config.json');
+
+const data = new SlashCommandBuilder()
+    .setName('leaderboard')
+    .setDescription('Valorant Leaderboards')
+	.addStringOption(option =>
+		option.setName('region')
+			.setDescription('Region of the leaderboard')
+			.setRequired(true)
+			.addChoices(
+				{ name: 'Asia Pacific', value: 'ap' },
+				{ name: 'Europe', value: 'eu' },
+				{ name: 'North America', value: 'na' },
+				{ name: 'Local', value: 'local' },
+			))
+
+    .addStringOption(option =>
+        option.setName('update')
+            .setDescription('Latest data vs up to day old (for local)')
+            .setRequired(false)
+			.addChoices(
+				{ name: 'True', value: 'true' },
+				{ name: 'False', value: 'false' }
+			));
+
+const execute = async (interaction) => {
+	var region = interaction.options.getString('region');
+	var update = interaction.options.getString('update');
+
+    await interaction.deferReply()
+
+    lib.python_calls([fp + 'commands.py', 'leaderboard', region, update])
+        .then(async (result) => {
+            console.log('Python process output:\n', result);
+            console.log(typeof result)
+            await interaction.editReply('```' + result + '```');
+        })
+        .catch((error) => {
+            console.error('Error running Python process:', error);
+        });
+}
+
+module.exports = {
+	data: data,
+	execute
+}
